ecabb58bb75545a041aa0b64d61673f21b1b6767
Benjamin Grössing <mailtogroe@gmail.com>
2016-05-31 19:32:23 +0200

    Missing px unit
    Chrome shows CSS warnings because they unit is missing

--- js/progressive-image-loading.js
+++ js/progressive-image-loading.js
@@ -27,7 +27,7 @@
         maxWidth = aspectRatioPlaceholder.offsetWidth,
         maxHeight = aspectRatioPlaceholder.offsetHeight;
 
-        aspectRatioPlaceholder.setAttribute('style', 'max-width:'+maxWidth+'; max-height:'+maxHeight+';');
+        aspectRatioPlaceholder.setAttribute('style', 'max-width:'+maxWidth+'px; max-height:'+maxHeight+'px;');
 
 
         // get thumbnail height wight
